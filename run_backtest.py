fca96a08f91806fbf44a2a96bd326dc75e0ec895
Aww11 <belyakova.evdokiya@gmail.com>
2026-05-28 21:41:24 +0300

    first MVP

--- run_backtest.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from src.backtest.backtest import run_backtest
-
-df = pd.read_csv("output/lsi_output.csv", parse_dates=["date"])
-bt = run_backtest(df)
-bt.to_csv("output/backtest.csv", index=False)
-print(bt.to_string(index=False))
